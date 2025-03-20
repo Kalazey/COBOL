5a7a3d6fe879a2561ae3e03ab713cf5f3fb2dd16
Kalazey <maillot.mathieu62@gmail.com>
2025-03-20 23:20:11 +0100

    First program in COBOL

---
+++ HelloWorld.cbl
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HelloWorld.
+       AUTHOR. Mathieu.
+       
+       PROCEDURE DIVISION.
+           
+           DISPLAY 'HELLO WORLD'.
+
+           STOP RUN.
+           
